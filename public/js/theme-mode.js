d568f00f15f18d495ec92a91536e0c84cfcc0bd0
ayamir <lgt986452565@gmail.com>
2024-04-26 10:42:36 +0800

    Publishing to gh-pages (publish.sh)

--- public/js/theme-mode.js
+++ public/js/theme-mode.js
@@ -1,45 +1,54 @@
 function switchTheme() {
   const currentStyle = currentTheme();
-  const iconElement = document.getElementById('github-icon');
+  const iconElement = document.getElementById("github-icon");
 
-  if (currentStyle === 'light') {
-    setTheme('dark');
-    setIconTheme('dark')
+  if (currentStyle === "light") {
+    setTheme("dark");
+    setIconTheme("dark");
     if (iconElement) {
-      iconElement.setAttribute('class', 'octicon');
-      iconElement.setAttribute('color', '#f0f6fc');
+      iconElement.setAttribute("class", "octicon");
+      iconElement.setAttribute("color", "#f0f6fc");
     }
-  }
-  else {
-    setTheme('light');
-    setIconTheme('light')
+  } else {
+    setTheme("light");
+    setIconTheme("light");
     if (iconElement) {
-      iconElement.removeAttribute('color');
-      iconElement.removeAttribute('class');
+      iconElement.removeAttribute("color");
+      iconElement.removeAttribute("class");
     }
   }
 }
 
 function setTheme(style) {
-  document.querySelectorAll('.isInitialToggle').forEach(elem => {
-    elem.classList.remove('isInitialToggle');
+  document.querySelectorAll(".isInitialToggle").forEach((elem) => {
+    elem.classList.remove("isInitialToggle");
   });
-  document.documentElement.setAttribute('data-color-mode', style);
-  localStorage.setItem('data-color-mode', style);
+  document.documentElement.setAttribute("data-color-mode", style);
+  const message = {
+    type: "set-theme",
+    theme: style == "dark" ? "github-dark" : "github-light",
+  };
+  const iframe = document.querySelector(".utterances-frame");
+  iframe.contentWindow.postMessage(message, "https://utteranc.es");
+  localStorage.setItem("data-color-mode", style);
 }
 
 function setIconTheme(theme) {
-  twitterIconElement = document.getElementById('twitter-icon');
-  if (theme === 'light') {
-    twitterIconElement.setAttribute("fill", "black")
-  } else if (theme === 'dark') {
-    twitterIconElement.setAttribute("fill", "white")
+  twitterIconElement = document.getElementById("twitter-icon");
+  if (theme === "light") {
+    twitterIconElement.setAttribute("fill", "black");
+  } else if (theme === "dark") {
+    twitterIconElement.setAttribute("fill", "white");
   }
 }
 
 function currentTheme() {
-  const localStyle = localStorage.getItem('data-color-mode');
-  const systemStyle = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+  const localStyle = localStorage.getItem("data-color-mode");
+  const systemStyle =
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches
+      ? "dark"
+      : "light";
   return localStyle || systemStyle;
 }
 
